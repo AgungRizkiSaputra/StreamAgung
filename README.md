32becb44261b24a9668a7bc4c41408dcd61ef2ba
zahHhar <agungrzksptra80@gmail.com>
2025-04-24 18:36:56 +0700

    P2: Jawaban Soal 6

--- README.md
+++ README.md
@@ -42,3 +42,13 @@
 ### P1: Jawaban Soal 5
 
 listen itu langsung nanggepin data yang masuk, cocok buat update UI secara real-time. Sedangkan await for nunggu data satu-satu secara berurutan dalam fungsi async, lebih cocok buat proses data yang butuh ketertiban.
+
+# Praktikum 2 : Stream Controllers dan Sink
+
+### P2: Jawaban Soal 6
+
+- Langkah 8 berfungsi untuk membuat dan mengatur stream agar bisa nerima data (angka) secara real-time. Saat ada angka baru masuk ke stream, setState akan memperbarui UI untuk menampilkan angka terbaru lewat lastNumber.
+
+- Langkah 10 berfungsi membuat angka acak dari 0 sampai 9, lalu mengirimkannya ke stream melalui method addNumberToSink, sehingga angka itu bisa langsung ditangkap oleh listener di Langkah 8. Jadi, Langkah 8 menangani penerimaan data, dan Langkah 10 mengirimkan datanya.
+
+<img src="https://github.com/AgungRizkiSaputra/StreamAgung/blob/main/image/GIFP2soal6.gif"  width="150px" >
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'stream.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -27,13 +29,29 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  int lastNumber = 0;
+  late StreamController NumberStreamController;
+  late NumberStream numberStream;
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Stream')),
-      body: Container(decoration: BoxDecoration(color: bgColor)),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(lastNumber.toString()),
+            ElevatedButton(
+              onPressed: () => addRandomNumber(),
+              child: Text('New Random Number'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -52,8 +70,28 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   @override
   void initState() {
+    numberStream = NumberStream();
+    NumberStreamController = numberStream.controller;
+    Stream stream = NumberStreamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    });
     super.initState();
     colorStream = ColorStream();
     changeColor();
+  }
+
+  @override
+  void dispose() {
+    NumberStreamController.close();
+    super.dispose();
+  }
+
+  void addRandomNumber() {
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
   }
 }
